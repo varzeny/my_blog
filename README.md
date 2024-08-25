ba24e5ba2a5fd9861c7c79725ac0130726dc5bf3
varzeny <varzeny@gmail.com>
2024-08-25 17:52:51 +0900

    재시작....시발

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # my_blog
 
+## env
+django, daphne, mysql, nginx
+
+## install
+
+## activate
+daphne -b 0.0.0.0 -p 8001 myblog.asgi:application
--- myblog/blog/static/blog/js/home.js
+++
@@ -1,9 +0,0 @@
-// home.js
-
-document.addEventListener("DOMContentLoaded", init);
-
-async function init() {
-    
-
-    console.log("home.js load complete");
-}
